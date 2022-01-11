b660d4f374f924fce18f0465c15b3c5cd1d67edc
wasantac <34143200+wasantac@users.noreply.github.com>
2022-01-11 18:15:38 -0500

    uri

--- full-project/CET-backend/collections/index.js
+++ full-project/CET-backend/collections/index.js
@@ -1,7 +1,7 @@
 const nodbConfig = require("../config/nodb.config.js");
 const mongoose = require("mongoose");
-
-const db_path =  nodbConfig.dialect + "://" + nodbConfig.HOST + "/" + nodbConfig.noDB;
+const uri = "mongodb+srv://adminMongo:admin1234@cluster0.38mkx.mongodb.net/CET?retryWrites=true&w=majority";
+const db_path = nodbConfig.dialect + "://" + nodbConfig.HOST + "/" + nodbConfig.noDB;
 const config = {
   useNewUrlParser: true,
   useCreateIndex: false,
@@ -11,7 +11,7 @@ const config = {
 };
 
 mongoose
-  .connect(db_path, config)
+  .connect(uri, config)
   .then(() => console.log("DB connnection successful!"))
   .catch((err) => {
     console.error.bind(console, "MongoDB connection error:");
